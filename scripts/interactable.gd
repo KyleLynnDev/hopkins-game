f89f92fd25e2c7d6080e21c2802440ac57989d15
Kyle Lynn <lynninlcn@gmail.com>
2025-04-05 02:14:53 -0700

    Implemented interaction system

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,5 +1,7 @@
-extends Node3D
+extends StaticBody3D
 
+@export var interact_name: String = "Unknown Object" 
 
 func interact():
-	print("Interacted with ", self.name)
+	print("You interacted with:", interact_name)
+	# TODO: launch UI, show info panels, etc.
